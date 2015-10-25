8ad73622d9edbaf870d26ec75d7c3daa65111f9e
Sachin Virdy <svirdy1@gmail.com>
2015-10-24 19:36:48 -0700

    layout and timer function

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = NSTimer()
+    var count = 0
+    
+    func updateTime() {
+        count++
+        time.text = "\(count)"
+    }
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    
+    @IBOutlet var time: UILabel!
+    
+    @IBAction func stop(sender: AnyObject) {
+        timer.invalidate()
+        
+        count = 0
+        
+        time.text = "0"
+    }
+    
+    @IBAction func play(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
